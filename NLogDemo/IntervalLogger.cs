c533035795901abb59c498f03a7bc37802441198
kielchang <kiel.chang.w@gmail.com>
2020-04-09 16:57:18 +0800

    add NLogDemo

---
+++ NLogDemo/IntervalLogger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace NLogDemo
+{
+    class IntervalLogger
+    {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+        private static Timer timer;
+
+        public IntervalLogger() {
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Elapsed += OnTimedEvent;
+            timer.AutoReset = true;
+        }
+
+        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            Logger.Info("Hello world");
+            Console.WriteLine($"Add Log at {e.SignalTime}");
+        }
+
+        public void start() {
+            timer.Enabled = true;
+        }
+
+        public void stop() {
+            timer.Enabled = false;
+        }
+    }
+}
---
+++ NLogDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntervalLogger intervalLogger = new IntervalLogger();
+            intervalLogger.start();
+
+            Console.WriteLine("Press the Enter key to exit the program at any time... ");
+            Console.ReadLine();
+            intervalLogger.stop();
+        }
+    }
+}
